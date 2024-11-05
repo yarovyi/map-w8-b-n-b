697516112ac5e95e2e3a89e8b6eef16b30e039df
Denys Yarovyi <denys.yarovyi@gmail.com>
2024-11-05 18:20:58 +0000

    Мапа позеленіла

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VkaWxrb3Zza3kiLCJhIjoiY20zNGI3ejNsMXBpYjJpc2FrNzRiMmoxbCJ9.mP73e1WrGJUSHriWgvyLPg';
 const map = new mapboxgl.Map({
     container: 'map', // ID контейнера
-    style: 'mapbox://styles/mapbox/dark-v11', // Стиль мапи
+    style: 'mapbox://styles/mapbox/satellite-streets-v12', // Стиль мапи
     center: [30.5234, 50.4501], // Координати для центру карти (Київ)
     zoom: 10 // Рівень масштабу
 });
